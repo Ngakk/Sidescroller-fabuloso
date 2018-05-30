0ee349004f6e427baffad5d008a86d6fd1164842
Ngak <ngakwastaken@gmail.com>
2018-05-30 12:56:02 -0500

    camera shake y shake en caida

---
+++ Assets/_GameFiles/Betatesting/camera/BasicMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour {
+
+    public Camera cam;
+    public float speed;
+    public float lowerShakeThreshold;
+    Rigidbody rigi;
+    int dirX;
+
+	// Use this for initialization
+	void Start () { 
+        rigi = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+            dirX = -1;
+        else if (Input.GetKey(KeyCode.D))
+            dirX = 1;
+        else
+            dirX = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rigi.AddForce(Vector3.up * speed, ForceMode.Impulse);
+
+        rigi.velocity += new Vector3(dirX * speed * Time.deltaTime, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.relativeVelocity.magnitude > lowerShakeThreshold)
+            cam.GetComponent<CameraShake>().AddTrauma(collision.relativeVelocity.magnitude/50);
+    }
+}
---
+++ Assets/_GameFiles/Betatesting/camera/CameraShake.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+    float trauma;
+    public float traumaHealRate;
+    public float MaxOffsetXshake, MaxOffsetYshake;
+    float xOffsetShake, yOffsetShake;
+    Vector3 previous;
+
+    // Use this for initialization
+    void Start()
+    {
+        previous = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+            AddTrauma(0.2f);
+
+        Shake();
+
+        trauma -= traumaHealRate * Time.deltaTime;
+        if (trauma < 0)
+            trauma = 0;
+    }
+
+    void Shake()
+    {
+        gameObject.transform.position = previous;
+        previous = transform.position;
+        xOffsetShake = MaxOffsetXshake * GetTrauma() * Random.Range(-1, 1);
+        yOffsetShake = MaxOffsetYshake * GetTrauma() * Random.Range(-1, 1);
+
+        gameObject.transform.Translate(xOffsetShake, yOffsetShake, 0);
+    }
+
+    float GetTrauma()
+    {
+        return trauma * trauma;
+    }
+
+    public void AddTrauma(float t)
+    {
+        trauma += t;
+    }
+}
