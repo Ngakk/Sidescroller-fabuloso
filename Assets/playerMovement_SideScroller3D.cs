6b593c07dcbcf246ad96fa52803b388d49ea07b5
Galaxyben <galaxyben1@gmail.com>
2018-06-04 17:02:09 -0500

    non_playable movement

---
+++ Assets/playerMovement_SideScroller3D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement_SideScroller3D : MonoBehaviour {
+    public float maximumSpeed;
+    public float maxAcceleration;
+    public float jumpForce;
+    private Rigidbody rigi;
+    private float m_Horizontal;
+    private bool m_Jump;
+
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        m_Horizontal = Input.GetAxis("Horizontal");
+        m_Jump = Input.GetButton("Jump");
+
+        if(m_Jump == true)
+        {
+            rigi.velocity += new Vector3();
+        }
+
+        if(m_Horizontal != 0)
+        {
+            rigi.velocity += new Vector3(maxAcceleration * m_Horizontal * Time.deltaTime, 0.0f, 0.0f);
+            if(rigi.velocity.x >= maximumSpeed)
+            {
+                rigi.velocity = new Vector3(maximumSpeed, rigi.velocity.y, rigi.velocity.z);
+            }
+            else if (rigi.velocity.x <= -maximumSpeed)
+            {
+                rigi.velocity = new Vector3(-maximumSpeed, rigi.velocity.y, rigi.velocity.z);
+            }
+        }
+        
+    }
+
+}
