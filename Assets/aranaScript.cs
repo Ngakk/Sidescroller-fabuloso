384b30ce1772c8ae7f421b95bb6203f13431ce75
Galaxyben <galaxyben1@gmail.com>
2018-06-26 21:40:32 -0500

    Enemigos&Escenario

--- Assets/aranaScript.cs
+++ Assets/aranaScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class aranaScript : MonoBehaviour {
+    public int Elife = 15;
     private Animator Anim;
     private Rigidbody Rigi;
     private Vector3 moveDirection;
@@ -17,29 +18,30 @@ public class aranaScript : MonoBehaviour {
     void Start () {
         Rigi = GetComponent<Rigidbody>();
         Anim = GetComponentInChildren<Animator>();
+        Anim.SetTrigger("walk");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Rigi.velocity += new Vector3(enemieCurrentAcceleration * dir * Time.deltaTime, 0f, 0f);
-        if (Rigi.velocity.x >= enemieMaxSpeed)
-        {
+        if (Rigi.velocity.x >= enemieMaxSpeed) {
             Rigi.velocity = new Vector3(enemieMaxSpeed, Rigi.velocity.y, Rigi.velocity.z);
-        }
-        else if (Rigi.velocity.x <= -enemieMaxSpeed)
-        {
+        } else if (Rigi.velocity.x <= -enemieMaxSpeed) {
             Rigi.velocity = new Vector3(-enemieMaxSpeed, Rigi.velocity.y, Rigi.velocity.z);
         }
 
-        if (changeDir)
-        {
+        if (changeDir) {
             Quaternion rot = Quaternion.Euler(0, 90, 0);
             gameObject.transform.rotation = rot;
-        }
-        else
-        {
+        } else {
             Quaternion rot = Quaternion.Euler(0, -90, 0);
             gameObject.transform.rotation = rot;
+        }
+
+        if(Elife <= 0)
+        {
+            Anim.SetTrigger("walk");
+            EnemieDead();
         }
     }
 
@@ -51,5 +53,24 @@ public class aranaScript : MonoBehaviour {
             changeDir = !changeDir;
         }
 
+        if (_col.gameObject.CompareTag("bullet"))
+            Elife = Elife - 5;
+
+        if (_col.gameObject.CompareTag("bullet2"))
+            Elife = Elife - 2;
+    }
+
+    void OnCollisionEnter(Collision _col)
+    {
+        if(_col.gameObject.CompareTag("Player"))
+        {
+            Anim.SetTrigger("Atack");
+        }
+    }
+
+    public void EnemieDead()
+    {
+        dir = 0;
+        Anim.SetTrigger("die");
     }
 }
