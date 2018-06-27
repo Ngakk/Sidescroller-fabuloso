bbd83dfbf768ff9d93fdb24af4f751c2a8eb37e1
Ngakk <ngakwastaken@gmail.com>
2018-06-26 22:23:57 -0500

    El Paralax mas vergas de tlaquepaque

---
+++ Assets/_GameFiles/Escenario/Back and Foregrounds/Paralax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour {
+
+	public Camera cam;
+	public float zDistance;
+	public GameObject center;
+	public float xOffset;
+	public float yOffset;
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(center.transform.position.x, center.transform.position.y, -20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xOffset = (cam.transform.position.x - center.transform.position.x) / zDistance;
+		yOffset = (cam.transform.position.y - center.transform.position.y) / zDistance;
+		transform.position = new Vector3 (
+				center.transform.position.x + xOffset,
+				center.transform.position.y + yOffset,
+				-60);
+	}
+}
