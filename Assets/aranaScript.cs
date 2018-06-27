a6761772ad9047cf9531cb8d3e36ee6ffdb30f6d
Galaxyben <galaxyben1@gmail.com>
2018-06-26 19:19:50 -0500

    Escenario Y Enemigos pt.1

---
+++ Assets/aranaScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aranaScript : MonoBehaviour {
+    public Animator Anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
