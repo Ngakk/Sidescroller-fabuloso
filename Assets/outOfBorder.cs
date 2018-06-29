bd94dec5ac4e5cdc842b604ba72d1045ac919bcd
Galaxyben <galaxyben1@gmail.com>
2018-06-29 10:35:58 -0500

    awdaw

---
+++ Assets/outOfBorder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class outOfBorder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision _col)
+    {
+        if (_col.gameObject.CompareTag("Player"))
+            SceneManager.LoadScene(2);
+    }
+}
